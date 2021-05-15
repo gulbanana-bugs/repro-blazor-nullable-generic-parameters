24f4228b493c5f59c5ecd9ddd23c411d8f7caece
Thomas Castiglione <castiglione@mac.com>
2021-05-15 22:47:32 +0800

    remove unneeded files

--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using repro_blazor_nullable_generic_parameters.Data;
 
 namespace repro_blazor_nullable_generic_parameters
 {
@@ -28,7 +27,6 @@ namespace repro_blazor_nullable_generic_parameters
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
